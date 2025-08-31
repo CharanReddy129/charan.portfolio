d990be1cbfedf59af61df41386556b8deadb294a
charan <charan@actions.com>
2025-08-31 12:02:10 +0530

    added comment in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // base: mode === 'production' ? "/charan.portfolio/" : "/",
   base: "/charan.portfolio/",
   server: {
     host: "::",
